46253562bbfe42b427bf16ed153deeda6a08a65a
HimariO <dsfhe49854@gmail.com>
2023-07-06 00:21:17 +0800

    Reinplement drag diffusion base on pipeline from efficient-prompt-to-prompt

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import torch
 import requests
 import matplotlib.pyplot as plt
+from functools import partial
 
 from PIL import Image, ImageDraw
 from io import BytesIO
 from diffusers.schedulers import DDIMInverseScheduler
-from drag_pipeline import DragDiffusionPipeline, MotionSup
-from inv_pipeline import StableDiffusionInversePipeline
+
+from drag_inpaint_pipeline import DragDiffusionInpaintPipeline, prepare_mask_and_masked_image
+from drag_pipeline import DragDiffusionPipeline, load_img
+from motion_sup import MotionSup, unet_feat_hook
 
 
 
@@ -39,10 +42,10 @@ def test_motion_super():
     plt.show()
 
 
-def run_drag_diffusion():
+def run_drag_inpaint_diffusion():
     lora_model_path = "./sd-model-lora"
 
-    pipeline = DragDiffusionPipeline.from_pretrained(
+    pipeline = DragDiffusionInpaintPipeline.from_pretrained(
         "runwayml/stable-diffusion-v1-5",
         torch_dtype=torch.float16,
     )
@@ -60,8 +63,21 @@ def run_drag_diffusion():
     handle_points_pt = (handle_points_pt - 0.5) * 2
     target_points_pt = (target_points_pt - 0.5) * 2
 
+    # inv_pipeline = DragDiffusionPipeline(
+    #     vae=pipeline.vae,
+    #     text_encoder=pipeline.text_encoder,
+    #     tokenizer=pipeline.tokenizer,
+    #     unet=pipeline.unet,
+    #     scheduler=pipeline.scheduler,
+    # )
 
     prompt = "A dog sitting on the bench in the park"
+    # src_image = load_img("asset/lora/train/overture-creations-5sI6fQgYIuo.png", target_size=512)
+    # image_latents = inv_pipeline.get_image_latents(
+    #     src_image.unsqueeze(0).to("cuda").half())
+    # text_embeddings = inv_pipeline.get_text_embedding(prompt)
+
+
     image = pipeline(
         prompt=prompt,
         image=init_image,
@@ -72,17 +88,98 @@ def run_drag_diffusion():
         strength=0.7,
     ).images[0]
 
-    # image = pipeline(
-    #     prompt=prompt,
-    #     image=init_image,
-    #     mask_image=mask_image,
-    #     handle_points=handle_points_pt,
-    #     target_points=target_points_pt,
-    #     num_inference_steps=50
-    # )[0]
-
     # inv_latent, image = pipeline.invert(prompt, image=init_image)
     # image = pipeline.invert(prompt, image=init_image, guidance_scale=.99, num_inference_steps=10)[1][0]
+    draw = ImageDraw.Draw(init_image, )
+    for y, x in handle_points:
+        draw.ellipse((x-5, y-5, x+5, y+5), fill = 'blue', width=10)
+    for y, x in target_points:
+        draw.ellipse((x-5, y-5, x+5, y+5), fill = 'red', width=10)
+    # init_image.show()
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(init_image)
+    plt.subplot(1, 2, 2)
+    plt.imshow(image)
+    plt.show()
+
+
+def run_drag_diffusion():
+    lora_model_path = "./sd-model-lora"
+
+    pipeline = DragDiffusionInpaintPipeline.from_pretrained(
+        "runwayml/stable-diffusion-v1-5",
+        torch_dtype=torch.float16,
+    )
+    # pipeline.inverse_scheduler = DDIMInverseScheduler.from_config(pipeline.scheduler.config)
+    pipeline.unet.load_attn_procs(lora_model_path)
+    pipeline = pipeline.to("cuda")
+
+    init_image = Image.open("asset/lora/train/overture-creations-5sI6fQgYIuo.png").resize((512, 512))
+    mask_image = Image.open("asset/overture-creations-5sI6fQgYIuo_mask.png").resize((512, 512))
+
+    handle_points = [[150, 245], [150, 270], [170, 255]]
+    target_points = [[200, 240], [200, 265], [220, 250]]
+    handle_points_pt = torch.tensor([[x / 512, y / 512] for y, x in handle_points])
+    target_points_pt = torch.tensor([[x / 512, y / 512] for y, x in target_points])
+    handle_points_pt = (handle_points_pt - 0.5) * 2
+    target_points_pt = (target_points_pt - 0.5) * 2
+
+    inv_pipeline = DragDiffusionPipeline(
+        vae=pipeline.vae,
+        text_encoder=pipeline.text_encoder,
+        tokenizer=pipeline.tokenizer,
+        unet=pipeline.unet,
+        scheduler=pipeline.scheduler,
+    )
+
+    prompt = "A dog sitting on the bench in the park"
+    # src_image = load_img("asset/lora/train/overture-creations-5sI6fQgYIuo.png", target_size=512)
+    src_mask, src_masked, src_image = prepare_mask_and_masked_image(init_image, mask_image, 512, 512, return_image=True)
+    src_mask = torch.nn.functional.interpolate(src_mask, size=(64, 64))
+    
+    image_latents = inv_pipeline.get_image_latents(
+        src_image.to("cuda").half())
+    text_embeddings = inv_pipeline.get_text_embedding(prompt)
+    
+    reversed_latents = inv_pipeline.forward_diffusion(
+        latents=image_latents,
+        text_embeddings=text_embeddings,
+        guidance_scale=1,
+        num_inference_steps=50,
+        early_stop_step=40,
+    )
+
+    drag_reverse_latents = inv_pipeline.tune_latent(
+        reversed_latents.clone(),
+        (1 - src_mask).to('cuda'), 
+        inv_pipeline.timesteps_tensor[-1].clone(),
+        text_embeddings.clone(),
+        {},
+        handle_points_pt.to('cuda'),
+        target_points_pt.to('cuda'),
+        steps=120,
+    )
+
+    print("Drag Diff: ", torch.abs(drag_reverse_latents - reversed_latents).mean())
+    
+    reconstructed_latents = inv_pipeline.backward_diffusion(
+        latents=drag_reverse_latents,
+        text_embeddings=text_embeddings,
+        guidance_scale=1,
+        num_inference_steps=50,
+    )
+    
+
+    # helper function to show images
+    def latents_to_imgs(latents):
+        x = inv_pipeline.decode_image(latents)
+        x = inv_pipeline.torch_to_numpy(x)
+        x = inv_pipeline.numpy_to_pil(x)
+        return x
+    
+    image = latents_to_imgs(reconstructed_latents)[0]
+
     draw = ImageDraw.Draw(init_image, )
     for y, x in handle_points:
         draw.ellipse((x-5, y-5, x+5, y+5), fill = 'blue', width=10)
---
+++ motion_sup.py
@@ -0,0 +1,132 @@
+from itertools import product
+
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+from torch import Tensor
+from termcolor import colored
+
+def unet_feat_hook(pipeline, module, xin, xout):
+    pipeline.unet_feat_cache = xout
+
+
+class MotionSup:
+    """
+    Motion supervision for 1 image
+    """
+    
+    def __init__(
+            self, 
+            src_points: Tensor,
+            dst_points: Tensor,
+            refernce_feat: Tensor,
+            refernce_latent: Tensor,
+            mask: Tensor,
+            steps=150,
+            debug_log=True,
+        ):
+        self.init_latent = refernce_latent.float().clone().detach()
+        self.ref_latent = torch.nn.Parameter(refernce_latent.float())
+        self.ref_feat = refernce_feat.clone().detach()
+        self.mask = mask.float()
+        self.adam = torch.optim.Adam([self.ref_latent], lr=1e-2)
+        
+        self.steps = steps
+        self._debug_log = debug_log
+        
+        if not ((src_points <= 1).all() and (src_points >= -1).all()):
+            raise ValueError()
+        if not ((dst_points <= 1).all() and (dst_points >= -1).all()):
+            raise ValueError()
+        self.src_points = src_points
+        self.dst_points = dst_points
+        self.handle_points = src_points.clone()
+        
+        c, h, w = refernce_feat.shape
+        self.radius = max(1, min(h, w) // 8)
+        self.sample_offsets = self._radius_offset(1)
+        self.search_offsets = self._radius_offset(self.radius, include_origin=True)
+    
+    def _radius_offset(self, radius, include_origin=True):
+        c, h, w = self.ref_feat.shape
+        offsets = [
+            [x / w, y / h]
+            for y, x in product(
+                range(-radius, radius + 1),
+                range(-radius, radius + 1))
+            if (y**2 + x**2)**0.5 <= radius and (not(x == 0 and y == 0) or include_origin)
+        ]
+        offsets = torch.tensor(
+            offsets,
+            dtype=self.src_points.dtype,
+            device=self.src_points.device
+        )
+        return offsets
+    
+    def _radius_sample(self, feat_map, points, offsets):
+        n = points.shape[0]
+        points = points.reshape([1, 1, n, 2])
+        points = points.repeat([1, len(offsets), 1, 1])
+        points += offsets.reshape([1, len(offsets), 1, 2])
+        feats = F.grid_sample(feat_map, points)
+        return feats[0]
+    
+    def _print(self, *args):
+        if self._debug_log:
+            print(*args)
+    
+    @torch.no_grad()
+    def search_handle(self, prev_feat: Tensor, feat_map: Tensor):
+        n = self.src_points.shape[0]
+        feat_map = torch.unsqueeze(feat_map, 0).float()
+        prev_feat = torch.unsqueeze(prev_feat, 0).float()
+        
+        # src_pts_4d = self.src_points.reshape([1, 1, n, 2])
+        src_pts_4d = self.handle_points.reshape([1, 1, n, 2])
+        src_feats = F.grid_sample(prev_feat, src_pts_4d)
+        src_feats = rearrange(src_feats, "b c h w -> (b h w) c")
+
+        offsets = self.search_offsets
+        radius_feats = self._radius_sample(feat_map, self.handle_points, offsets)
+        radius_feats = rearrange(radius_feats, "c offsets pts -> pts offsets c")
+        
+        new_handles = []
+        for i, (src_pt, src_feat, rad_feat) in enumerate(zip(self.handle_points, src_feats, radius_feats)):
+            A = F.normalize(src_feat.unsqueeze(0), dim=1)
+            B = F.normalize(rad_feat, dim=1)
+            dot = A @ B.T
+            nearest = torch.argmax(dot, dim=1)
+            new_handles.append(src_pt + offsets[nearest[0]])
+            self._print(f"handle point[{i}] moved: ", offsets[nearest[0]])
+        
+        self.handle_points = torch.stack(new_handles)
+        self._print(
+            "abs(handle_points - dst_points) = ", 
+            torch.abs(self.handle_points - self.dst_points).sum(dim=1).cpu().numpy(),
+            torch.abs(self.ref_latent - self.init_latent).mean().cpu().numpy(),
+        )
+
+    def step(self, feat_map: Tensor, prev_feat=None, reset_backbound=False):
+        c, h, w = feat_map.shape
+        offsets = self.sample_offsets
+        feat_map = torch.unsqueeze(feat_map, dim=0).float()
+        prev_feat = torch.unsqueeze(prev_feat, dim=0).float() if prev_feat is not None else feat_map
+        
+        src_feats = self._radius_sample(prev_feat, self.handle_points, offsets)
+
+        norm_direct = F.normalize(self.dst_points - self.handle_points)
+        track_points = self.handle_points + norm_direct * 2 / min(h, w) * 4  # NOTE: try to move one "pixel" toward dst point on feature map
+        # track_points = self.handle_points + (self.dst_points - self.handle_points) / self.steps
+        tar_feats = self._radius_sample(feat_map, track_points, offsets)
+        
+        self.adam.zero_grad()
+        motion_l1_loss = F.l1_loss(tar_feats, src_feats.detach())
+        motion_l1_loss += F.l1_loss(self.ref_latent * self.mask, self.init_latent * self.mask) * 0.1
+        motion_l1_loss.backward()
+        self.adam.step()
+        
+        if reset_backbound:
+            self.ref_latent.data = self.ref_latent * (1 - self.mask) + self.init_latent * self.mask
+        
+        self._print(colored("motion_l1_loss: ", color='green'), float(motion_l1_loss))
+        return float(motion_l1_loss.detach().cpu())
